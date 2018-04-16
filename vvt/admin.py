b31ad579e3cc49043a6f3a331703775abc53309f
payerhans <32729314+payerhans@users.noreply.github.com>
2018-04-16 16:12:00 +0200

    erster view, bootstrap4 integration

--- vvt/admin.py
+++ vvt/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Datengruppen, Daten, Datenuebermittlung, Empfaenger
 
+class DatenInline(admin.StackedInline):
+    model = Daten
+    fields = ['daten']
+    extra = 3
+
+class DatenGruppenAdmin(admin.ModelAdmin):
+    #model = Datengruppen
+    fields = ['datengruppe']
+
+    inlines = [DatenInline]
+
 # Register your models here.
-admin.site.register(Datengruppen)
+#admin.site.register(Datengruppen)
 admin.site.register( Daten,)
 admin.site.register(Datenuebermittlung)
 admin.site.register(Empfaenger)
+admin.site.register(Datengruppen, DatenGruppenAdmin)
--- vvt/views.py
+++ vvt/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Datengruppen
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Das ist der VVT Index")
+    #datengruppen_list = [1,2,3,4,5,6]
+    datengruppen_list = Datengruppen.objects.all()
+    #output = ', '.join([q.datengruppe for q in datengruppen_list])
+    context = {'datengruppen' : datengruppen_list}
+    #return HttpResponse(output)
+    return render(request, 'index.html', context)
